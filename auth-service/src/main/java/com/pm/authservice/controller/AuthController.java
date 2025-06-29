88e4249ff1c56a9231cc19c8f1591dfb9ade304f
yashyogeshkathane <yashyogeshkathane@gmail.com>
2025-06-29 20:26:15 +0530

    added authorization code

---
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> tokenOptional=authService.authenticate(loginRequestDTO);
+
+        if(tokenOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token=tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+
+    }
+
+
+
+}
