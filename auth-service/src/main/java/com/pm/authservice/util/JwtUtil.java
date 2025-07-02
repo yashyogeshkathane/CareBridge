9fe3160cde4df595af0f58ec4836f201717be0d8
yashyogeshkathane <yashyogeshkathane@gmail.com>
2025-07-02 22:03:39 +0530

    added jwt logic

---
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.pm.authservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private final Key secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Base64.getDecoder()
+                .decode(secret.getBytes(StandardCharsets.UTF_8));
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email,String role){
+        return Jwts.builder()
+                .subject(email)
+                .claim("role",role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+                .signWith(secretKey)
+                .compact();
+
+    }
+
+}
